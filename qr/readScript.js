50a74707a065b383ea57c2d6fb84c8dfbf6cfee2
KarmaConfigs <karmaconfigs@gmail.com>
2020-03-31 23:32:52 +0200

    Update readScript.js

--- qr/readScript.js
+++ qr/readScript.js
@@ -4,7 +4,7 @@ function getQR() {
 	return url_string[2];
 }
 function getName() {
-    var url_string = window.location.href.toString().split('?');
+    var url_string = window.location.href.toString().split("?");
 
     return url_string[1];
 }
